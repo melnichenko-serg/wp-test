bd689f37c9cc3d3598bbc236eef9aa826ba75574
Programel <programel.dev@gmail.com>
2017-04-20 00:51:48 +0300

    start
    (cherry picked from commit d4fac1d)

---
+++ wp-content/themes/custom-theme/index.php
@@ -0,0 +1,79 @@
+<!-- Header -->
+<?php get_header(); ?>
+
+<section class="error_top_bg secondary-bg">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="top_banner">
+                    <div class="category_m">
+                        <h2> App News</h2>
+                        <div class="breadcrumb_m">
+                            <ul class="breadcrumbs_m" id="breadcrumbs_m">
+                                <li><a href="#"><i class="fa fa-home" aria-hidden="true"></i> Home </a></li>
+                                <li> /</li>
+                                <li><a href="#"> Blog</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="post_blog_bg primary-bg">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="col-md-8">
+					<?php if ( have_posts() ) : ?>
+						<?php while ( have_posts() ) : the_post(); ?>
+                            <article class="blog_post">
+                                <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+                                <div class="blog_category">
+                                    <ul>
+                                        <li><?php the_category(', '); ?></li>
+                                    </ul>
+                                </div>
+                                <div class="blog_text">
+                                    <ul>
+                                        <li> |</li>
+                                        <li>Post By : <?php the_author_posts_link(); ?></li>
+                                        <li> |</li>
+                                        <li> On : <?php the_date(); ?></li>
+                                    </ul>
+                                </div>
+                                <div class="blog_post_img">
+                                    <a href="<?php the_permalink(); ?>"> <img src="images/blog_post_img1.png" alt="image"> </a>
+                                </div>
+                                <p> Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical
+                                    Latin
+                                    literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at
+                                    Hampden-Sydney
+                                    College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage,
+                                    and
+                                    going through the cites of the word in classical literature, discovered the undoubtable source. </p>
+
+                                <a href="<?php the_permalink(); ?>"> Continue reading <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                            </article>
+						<?php endwhile; ?>
+					<?php endif; ?>
+
+                    <div class="next_page">
+                        <ul class="page-numbers">
+                            <li><span class="page-numbers current">1</span></li>
+                            <li><a href="#" class="page-numbers">2</a></li>
+                            <li><a href="#" class="page-numbers">3</a></li>
+                            <li><a href="#" class="page-numbers">4</a></li>
+                            <li><a href="#" class="next page-numbers">Next</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- Sidebar -->
+				<?php get_sidebar(); ?>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Footer -->
+<?php get_footer(); ?>
